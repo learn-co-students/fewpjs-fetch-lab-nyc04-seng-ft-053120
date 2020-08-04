fcd42622e072cb29249a6449ebab6ca425363d22
tracycollins <tc@threeceemedia.com>
2020-08-04 14:06:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json)
+    );
 }
 
 function renderBooks(books) {
@@ -11,6 +14,6 @@ function renderBooks(books) {
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks()
 })
